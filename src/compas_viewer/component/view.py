4ee09d93b26516f84d2c376a6818297689c34cdb
Zac Zhang <zac.z.zhang@outlook.com>
2023-12-07 11:00:11 +0100

    push the file structure after the discussion

--- src/compas_viewer/component/view.py
+++
@@ -1,3 +0,0 @@
-"""
-Base class for all views.
-"""
--- src/compas_viewer/views/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Views are abstracted view ports that can be added to the viewer. 
-They can be used to display 3D geometry, menus, forms, 2D plots, or other information.
-"""
