31f2b240492c638fdbeec5e519fda37835cc28ca
Zac Zhang <zac.z.zhang@outlook.com>
2023-12-07 16:56:39 +0100

    format

--- tests/configurations/test_config.py
+++ tests/configurations/test_config.py
@@ -4,23 +4,23 @@ from compas_viewer.configurations import ViewerConfig
 
 def test_viewer_config() -> None:
     config = ViewerConfig.from_default()
-    assert type(config) == ViewerConfig
-    assert type(config.about) == str
-    assert type(config.title) == str
-    assert type(config.width) == int
-    assert type(config.height) == int
-    assert type(config.full_screen) == bool
+    assert isinstance(config, ViewerConfig)
+    assert isinstance(config.about, str)
+    assert isinstance(config.title, str)
+    assert isinstance(config.width, int)
+    assert isinstance(config.height, int)
+    assert isinstance(config.full_screen, bool)
     config.validate_data(config.data)
 
 
 def test_controller_config() -> None:
     config = ControllerConfig.from_default()
-    assert type(config) == ControllerConfig
-    assert type(config.pan) == dict
-    assert type(config.zoom) == dict
-    assert type(config.rotate) == dict
-    assert type(config.box_selection) == dict
-    assert type(config.selection) == str
-    assert type(config.multi_selection) == str
-    assert type(config.deletion) == str
+    assert isinstance(config, ControllerConfig)
+    assert isinstance(config.pan, dict)
+    assert isinstance(config.zoom, dict)
+    assert isinstance(config.rotate, dict)
+    assert isinstance(config.box_selection, dict)
+    assert isinstance(config.selection, str)
+    assert isinstance(config.multi_selection, str)
+    assert isinstance(config.deletion, str)
     config.validate_data(config.data)
