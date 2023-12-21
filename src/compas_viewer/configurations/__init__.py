e58618be80a33eb544115e94fedb99086682335f
Zac Zhang <zac.z.zhang@outlook.com>
2023-12-21 18:11:53 +0100

    lint

--- src/compas_viewer/configurations/__init__.py
+++ src/compas_viewer/configurations/__init__.py
@@ -1,4 +1,5 @@
 from .config import Config  # noqa: F401
 from .viewer_config import ViewerConfig, ViewerConfigType  # noqa: F401
 from .controller_config import ControllerConfig, ControllerConfigType  # noqa: F401
-from .render_config import RenderConfig, RenderConfigType, CameraConfig, CameraConfigType, RenderModeType, ViewModeType  # noqa: F401
+from .render_config import RenderConfig, RenderConfigType, CameraConfig, CameraConfigType  # noqa: F401
+from .render_config import RenderModeType, ViewModeType  # noqa: F401
