a11b227923f7a3f2c15ee7f2a4028ca913981964
Zac Zhang <zac.z.zhang@outlook.com>
2023-12-28 21:22:26 +0100

    update based on the comments

--- src/compas_viewer/actions/print_gl_info.py
+++ src/compas_viewer/actions/print_gl_info.py
@@ -1,21 +1,12 @@
-from typing import TYPE_CHECKING
-
-from compas_viewer.configurations import ActionConfig
 from compas_viewer.utilities import gl_info
 
 from .action import Action
-
-if TYPE_CHECKING:
-    from compas_viewer.viewer import Viewer
 
 
 class GLInfo(Action):
     """
     Pop up a window with OpenGL information.
     """
-
-    def __init__(self, name, viewer: "Viewer", config: ActionConfig):
-        super().__init__(name, viewer, config)
 
     def pressed(self):
         self.viewer.info(gl_info())
