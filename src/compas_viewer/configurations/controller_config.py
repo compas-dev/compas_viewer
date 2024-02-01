ad7d8602202f84bbe10b41262a9678d13e838155
Li <leochen1990@gmail.com>
2024-02-01 20:27:43 +0100

    lint

--- src/compas_viewer/configurations/controller_config.py
+++ src/compas_viewer/configurations/controller_config.py
@@ -124,7 +124,9 @@ class ControllerConfig(Config):
 
     @classmethod
     def from_default(cls) -> "ControllerConfig":
-        controller_config = ControllerConfig.from_json(Path(HERE, "configurations", "default_config", "controller.json"))
+        controller_config = ControllerConfig.from_json(
+            Path(HERE, "configurations", "default_config", "controller.json")
+        )
         if not isinstance(controller_config, ControllerConfig):
             raise TypeError(f"The {controller_config} is not a valid controller configuration file.")
         return controller_config
