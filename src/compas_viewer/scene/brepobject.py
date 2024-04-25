2c1a9491dfa106567003fee49dda290451897bce
Licini <leochen1990@gmail.com>
2024-04-25 15:41:20 +0200

    remove unnecessary try except

--- src/compas_viewer/scene/brepobject.py
+++ src/compas_viewer/scene/brepobject.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from compas_occ.brep import OCCBrep
+
 from compas.datastructures import Mesh
 from compas.geometry import Line
 from compas.geometry import Point
@@ -9,48 +11,43 @@ from compas.tolerance import TOL
 
 from .geometryobject import GeometryObject as ViewerGeometryObject
 
-try:
-    from compas_occ.brep import OCCBrep
 
-    class BRepObject(ViewerGeometryObject, GeometryObject):
-        """Viewer scene object for displaying COMPAS OCCBrep geometry.
+class BRepObject(ViewerGeometryObject, GeometryObject):
+    """Viewer scene object for displaying COMPAS OCCBrep geometry.
 
-        Attributes
-        ----------
-        brep : :class:`compas_occ.brep.OCCBrep`
-            The compas_occ Brep object.
-        mesh : :class:`compas.datastructures.Mesh`
-            The mesh representation of the Brep.
+    Attributes
+    ----------
+    brep : :class:`compas_occ.brep.OCCBrep`
+        The compas_occ Brep object.
+    mesh : :class:`compas.datastructures.Mesh`
+        The mesh representation of the Brep.
 
-        See Also
-        --------
-        :class:`compas_occ.brep.Brep`
-        """
+    See Also
+    --------
+    :class:`compas_occ.brep.Brep`
+    """
 
-        def __init__(self, brep: OCCBrep, **kwargs):
-            super().__init__(geometry=brep, **kwargs)
-            self.geometry: OCCBrep
-            self._viewmesh, self._boundaries = self.geometry.to_tesselation(TOL.lineardeflection)
+    def __init__(self, brep: OCCBrep, **kwargs):
+        super().__init__(geometry=brep, **kwargs)
+        self.geometry: OCCBrep
+        self._viewmesh, self._boundaries = self.geometry.to_tesselation(TOL.lineardeflection)
 
-        @property
-        def points(self) -> Optional[list[Point]]:
-            """The points to be shown in the viewer."""
-            return self.geometry.points
+    @property
+    def points(self) -> Optional[list[Point]]:
+        """The points to be shown in the viewer."""
+        return self.geometry.points
 
-        @property
-        def lines(self) -> Optional[list[Line]]:
-            """The lines to be shown in the viewer."""
-            lines = []
-            for polyline in self._boundaries:
-                for pair in pairwise(polyline.points):
-                    lines.append(Line(*pair))
+    @property
+    def lines(self) -> Optional[list[Line]]:
+        """The lines to be shown in the viewer."""
+        lines = []
+        for polyline in self._boundaries:
+            for pair in pairwise(polyline.points):
+                lines.append(Line(*pair))
 
-            return lines
+        return lines
 
-        @property
-        def viewmesh(self) -> Mesh:
-            """The mesh volume to be shown in the viewer."""
-            return self._viewmesh
-
-except ImportError:
-    pass
+    @property
+    def viewmesh(self) -> Mesh:
+        """The mesh volume to be shown in the viewer."""
+        return self._viewmesh
--- src/compas_viewer/scene/nurbssurfaceobject.py
+++ src/compas_viewer/scene/nurbssurfaceobject.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from compas_occ.brep import OCCBrep
+
 from compas.datastructures import Mesh
 from compas.geometry import Line
 from compas.geometry import NurbsSurface
@@ -10,42 +12,37 @@ from compas.tolerance import TOL
 
 from .geometryobject import GeometryObject as ViewerGeometryObject
 
-try:
-    from compas_occ.brep import OCCBrep
 
-    class NurbsSurfaceObject(ViewerGeometryObject, GeometryObject):
-        """Viewer scene object for displaying COMPAS NurbsSurface geometry.
+class NurbsSurfaceObject(ViewerGeometryObject, GeometryObject):
+    """Viewer scene object for displaying COMPAS NurbsSurface geometry.
 
-        See Also
-        --------
-        :class:`compas.geometry.NurbsSurface`
-        """
+    See Also
+    --------
+    :class:`compas.geometry.NurbsSurface`
+    """
 
-        def __init__(self, surface: NurbsSurface, **kwargs):
-            super().__init__(geometry=surface, **kwargs)
-            self.geometry: NurbsSurface
-            self._brep: OCCBrep = OCCBrep.from_surface(self.geometry)
-            self._viewmesh, self._boundaries = self._brep.to_tesselation(TOL.lineardeflection)
+    def __init__(self, surface: NurbsSurface, **kwargs):
+        super().__init__(geometry=surface, **kwargs)
+        self.geometry: NurbsSurface
+        self._brep: OCCBrep = OCCBrep.from_surface(self.geometry)
+        self._viewmesh, self._boundaries = self._brep.to_tesselation(TOL.lineardeflection)
 
-        @property
-        def points(self) -> Optional[list[Point]]:
-            """The points to be shown in the viewer."""
-            return self._brep.points
+    @property
+    def points(self) -> Optional[list[Point]]:
+        """The points to be shown in the viewer."""
+        return self._brep.points
 
-        @property
-        def lines(self) -> Optional[list[Line]]:
-            """The lines to be shown in the viewer."""
-            lines = []
-            for polyline in self._boundaries:
-                for pair in pairwise(polyline.points):
-                    lines.append(Line(*pair))
+    @property
+    def lines(self) -> Optional[list[Line]]:
+        """The lines to be shown in the viewer."""
+        lines = []
+        for polyline in self._boundaries:
+            for pair in pairwise(polyline.points):
+                lines.append(Line(*pair))
 
-            return lines
+        return lines
 
-        @property
-        def viewmesh(self) -> Mesh:
-            """The mesh volume to be shown in the viewer."""
-            return self._viewmesh
-
-except ImportError:
-    pass
+    @property
+    def viewmesh(self) -> Mesh:
+        """The mesh volume to be shown in the viewer."""
+        return self._viewmesh
