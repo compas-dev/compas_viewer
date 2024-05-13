8c601b3c3e7f549130302afdc4bfce9a65f500c0
Li <leochen1990@gmail.com>
2024-05-13 09:48:27 +0200

    Fixing singleton for subclasses

--- src/compas_viewer/singleton.py
+++ src/compas_viewer/singleton.py
@@ -2,10 +2,19 @@ class SingletonMeta(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
+        # Check if this class is a direct subclass of Singleton
+        if cls.__base__ is Singleton:
+            key_class = cls
+        else:
+            # Walk up the chain to find the first subclass of Singleton
+            key_class = cls
+            while key_class.__base__ is not Singleton:
+                key_class = key_class.__base__
+
+        if key_class not in cls._instances:
             instance = super().__call__(*args, **kwargs)
-            cls._instances[cls] = instance
-        return cls._instances[cls]
+            cls._instances[key_class] = instance
+        return cls._instances[key_class]
 
 
 class Singleton(metaclass=SingletonMeta):
