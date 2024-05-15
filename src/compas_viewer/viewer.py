c03564f151cd2d271966a72fa0774c2fde6275fd
pitsai <pinghsuntsai1122@gmail.com>
2024-05-15 09:50:01 +0200

    fix intervals

--- src/compas_viewer/viewer.py
+++ src/compas_viewer/viewer.py
@@ -2,22 +2,23 @@ import sys
 from typing import Callable
 from typing import Optional
 
+from PySide6.QtCore import QTimer
 from PySide6.QtWidgets import QApplication
 
+from compas_viewer.components.renderer import Renderer
 from compas_viewer.config import Config
 from compas_viewer.configurations import ControllerConfig
-from compas_viewer.controller import Controller
-from compas_viewer.components.renderer import Renderer
 from compas_viewer.configurations import RendererConfig
+from compas_viewer.controller import Controller
 from compas_viewer.scene.scene import ViewerScene
 from compas_viewer.singleton import Singleton
 from compas_viewer.ui.ui import UI
-from compas_viewer.qt import Timer
 
 
 class Viewer(Singleton):
     def __init__(self, *args, **kwargs):
         self.app = QApplication(sys.argv)
+        self.timer = QTimer()
         self.config = Config()
         self.scene = ViewerScene()
         # TODO(pitsai): combine config file
@@ -30,11 +31,11 @@ class Viewer(Singleton):
         self.ui.show()
         self.app.exec()
 
-    def on(self, interval: int, timeout: Optional[int] = None, frames: Optional[int] = None) -> Callable:
+    def on(self, interval: int, frames: Optional[int] = None) -> Callable:
+        """Decorator for callbacks of a dynamic drawing process with fixed intervals."""
         """Decorator for callbacks of a dynamic drawing process.
 
         Parameters
-        ----------
         interval : int
             Interval between subsequent calls to this function, in milliseconds.
         timeout : int, optional
@@ -46,44 +47,20 @@ class Viewer(Singleton):
         Returns
         -------
         Callable
-
-        Notes
-        -----
-        The difference between `interval` and `timeout` is that the former indicates
-        the time between subsequent calls to the callback,
-        without taking into account the duration of the execution of the call,
-        whereas the latter indicates a pause after the completed execution of the previous call,
-        before starting the next one.
-
-        Examples
-        --------
-        .. code-block:: python
-
-            angle = math.radians(5)
-
-
-            @viewer.on(interval=1000)
-            def rotate(frame):
-                obj.rotation = [0, 0, frame * angle]
-                obj.update()
-
         """
-        if (not interval and not timeout) or (interval and timeout):
-            raise ValueError("Must specify either interval or timeout.")
+        self.frame_count = 0
 
-        def outer(func: Callable):
-            def renderer():
-                func(self.frame_count)
-                self.renderer.update()
-                self.frame_count += 1
+        def decorator(func: Callable):
+            def wrapper():
                 if frames is not None and self.frame_count >= frames:
                     self.timer.stop()
+                    return
+                func(self.frame_count)
+                self.frame_count += 1
+                self.renderer.update()
 
-            if interval:
-                self.timer = Timer(interval=interval, callback=renderer)
-            if timeout:
-                self.timer = Timer(interval=timeout, callback=renderer, singleshot=True)
+            self.timer.timeout.connect(wrapper)
+            self.timer.start(interval)
+            return func
 
-            self.frame_count = 0
-
-        return outer
+        return decorator
