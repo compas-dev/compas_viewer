064be50e72e585bf9a6b04d02025bb6315366054
pitsai <pinghsuntsai1122@gmail.com>
2024-05-15 08:56:09 +0200

    add on method

---
+++ scripts/dynamic_mesh.py
@@ -0,0 +1,28 @@
+from random import random
+
+import compas
+from compas.colors import Color
+from compas.datastructures import Mesh
+from compas_viewer import Viewer
+
+viewer = Viewer()
+
+mesh = Mesh.from_off(compas.get("tubemesh.off"))
+obj = viewer.scene.add(mesh, surfacecolor=Color.cyan(), use_vertexcolors=False)
+
+
+
+@viewer.on(interval=1000)
+def deform_mesh(frame):
+    for v in mesh.vertices():
+        vertex: list = mesh.vertex_attributes(v, "xyz")  # type: ignore
+        vertex[0] += random() - 0.5
+        vertex[1] += random() - 0.5
+        vertex[2] += random() - 0.5
+        mesh.vertex_attributes(v, "xyz", vertex)
+    obj.init()
+    obj.update()
+    print(frame)
+
+
+viewer.show()
--- src/compas_viewer/viewer.py
+++ src/compas_viewer/viewer.py
@@ -1,15 +1,18 @@
 import sys
+from typing import Callable
+from typing import Optional
 
 from PySide6.QtWidgets import QApplication
 
-from compas_viewer.components.renderer import Renderer
 from compas_viewer.config import Config
 from compas_viewer.configurations import ControllerConfig
-from compas_viewer.configurations import RendererConfig
 from compas_viewer.controller import Controller
+from compas_viewer.components.renderer import Renderer
+from compas_viewer.configurations import RendererConfig
 from compas_viewer.scene.scene import ViewerScene
 from compas_viewer.singleton import Singleton
 from compas_viewer.ui.ui import UI
+from compas_viewer.qt import Timer
 
 
 class Viewer(Singleton):
@@ -26,3 +29,61 @@ class Viewer(Singleton):
         self.ui.lazy_init()
         self.ui.show()
         self.app.exec()
+
+    def on(self, interval: int, timeout: Optional[int] = None, frames: Optional[int] = None) -> Callable:
+        """Decorator for callbacks of a dynamic drawing process.
+
+        Parameters
+        ----------
+        interval : int
+            Interval between subsequent calls to this function, in milliseconds.
+        timeout : int, optional
+            Timeout between subsequent calls to this function, in milliseconds.
+        frames : int, optional
+            The number of frames of the process.
+            If no frame number is provided, the process continues until the viewer is closed.
+
+        Returns
+        -------
+        Callable
+
+        Notes
+        -----
+        The difference between `interval` and `timeout` is that the former indicates
+        the time between subsequent calls to the callback,
+        without taking into account the duration of the execution of the call,
+        whereas the latter indicates a pause after the completed execution of the previous call,
+        before starting the next one.
+
+        Examples
+        --------
+        .. code-block:: python
+
+            angle = math.radians(5)
+
+
+            @viewer.on(interval=1000)
+            def rotate(frame):
+                obj.rotation = [0, 0, frame * angle]
+                obj.update()
+
+        """
+        if (not interval and not timeout) or (interval and timeout):
+            raise ValueError("Must specify either interval or timeout.")
+
+        def outer(func: Callable):
+            def renderer():
+                func(self.frame_count)
+                self.renderer.update()
+                self.frame_count += 1
+                if frames is not None and self.frame_count >= frames:
+                    self.timer.stop()
+
+            if interval:
+                self.timer = Timer(interval=interval, callback=renderer)
+            if timeout:
+                self.timer = Timer(interval=timeout, callback=renderer, singleshot=True)
+
+            self.frame_count = 0
+
+        return outer
