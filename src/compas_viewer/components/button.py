8858065bf757de338b51403dc1f209e51c6f1b39
tomvanmele <tom.v.mele@gmail.com>
2024-05-21 11:12:29 +0200

    add text to button and convert to kwargs

--- src/compas_viewer/components/button.py
+++ src/compas_viewer/components/button.py
@@ -1,5 +1,7 @@
 import pathlib
 from typing import Callable
+from typing import Optional
+from typing import Union
 
 from PySide6 import QtCore
 from PySide6 import QtGui
@@ -12,9 +14,21 @@ def set_icon_path(icon_name: str) -> str:
 
 
 class Button(QtWidgets.QPushButton):
-    def __init__(self, icon_name: str, tooltip: str, action: Callable[[], None], parent=None):
+    def __init__(
+        self,
+        text: Optional[str] = None,
+        icon_path: Optional[Union[str, pathlib.Path]] = None,
+        tooltip: Optional[str] = None,
+        action: Optional[Callable[[], None]] = None,
+        parent=None,
+    ):
         super().__init__()
-        self.setIcon(QtGui.QIcon(set_icon_path(icon_name)))
-        self.setToolTip(tooltip)
+        if text:
+            self.setText(text)
+        if icon_path:
+            self.setIcon(QtGui.QIcon(set_icon_path(icon_path)))
+        if tooltip:
+            self.setToolTip(tooltip)
+        if action:
+            self.clicked.connect(action)
         self.setIconSize(QtCore.QSize(12, 12))
-        self.clicked.connect(action)
