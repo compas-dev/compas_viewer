08c9bf941f8e3d0e0af2b36a1aeec8d07dc177ec
tomvanmele <tom.v.mele@gmail.com>
2024-05-21 11:11:29 +0200

    pull up components

--- src/compas_viewer/components/__init__.py
+++ src/compas_viewer/components/__init__.py
@@ -1,1 +1,17 @@
-from .renderer import Renderer  # noqa: F401
+from .button import Button
+from .combobox import ComboBox
+from .combobox import ViewModeAction
+from .dialog import CameraSettingsDialog
+from .renderer import Renderer
+from .slider import Slider
+from .treeform import Treeform
+
+__all__ = [
+    "Button",
+    "ComboBox",
+    "CameraSettingsDialog",
+    "Renderer",
+    "Slider",
+    "Treeform",
+    "ViewModeAction",
+]
