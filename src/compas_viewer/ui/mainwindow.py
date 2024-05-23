9432d85e58d278a4a6340e4a6ae25c4990cadca4
tomvanmele <tom.v.mele@gmail.com>
2024-05-23 22:52:17 +0200

    same structure as others

--- src/compas_viewer/ui/mainwindow.py
+++ src/compas_viewer/ui/mainwindow.py
@@ -1,22 +1,11 @@
 from PySide6 import QtWidgets
-from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QMainWindow
 from PySide6.QtWidgets import QWidget
 
-from compas_viewer.base import Base
 
+class MainWindow:
 
-class MainWindow(QMainWindow, Base):
-    def lazy_init(self):
-        title = self.viewer.config.window.title
-        self.setWindowTitle(title)
-        layout = QtWidgets.QHBoxLayout()
-        central = QWidget()
-        central.setLayout(layout)
-        self.setCentralWidget(central)
-
-    def keyPressEvent(self, event: QKeyEvent) -> None:
-        print(event.key())
-        print(event.modifiers())
-        print(event.keyCombination())
-        return super().keyPressEvent(event)
+    def __init__(self, title):
+        self.title = title
+        self.widget = QMainWindow()
+        self.widget.setWindowTitle(title)
