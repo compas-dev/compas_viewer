6db8fd1512136f661dcebadbd5df04c807b757c4
pitsai <pinghsuntsai1122@gmail.com>
2024-05-28 18:15:20 +0200

    clean

--- src/compas_viewer/components/slider.py
+++ src/compas_viewer/components/slider.py
@@ -41,7 +41,7 @@ class Slider(QWidget):
             Orientation of the slider. True for horizontal, False for vertical. Defaults to True.
         starting_val : int, optional
             Initial value of the slider, defaults to the minimum value.
-        tick_interval : float, optional
+        tick_interval : int, optional
             Interval between tick marks. No ticks if None. Defaults to None.
 
         Attributes
@@ -70,24 +70,20 @@ class Slider(QWidget):
         >>> slider = Slider("Brightness", min_val=0, max_val=255, starting_val=100)
         """
         super().__init__()
-        # TODO(pitsai): add step method
         self.title = title
         self.action = action
         self._horizontal = horizontal
         self.min_val = min_val
         self.max_val = max_val
-        self.step = step
-        self.starting_val = starting_val
+        self.step = step or 1
+        self.starting_val = starting_val if starting_val is not None else self.min_val
         self._tick_interval = tick_interval
 
-        if self._horizontal:
-            orientation = Qt.Horizontal
-        else:
-            orientation = Qt.Vertical
+        orientation = Qt.Horizontal if horizontal else Qt.Vertical
 
         if self.step:
-            self.min_val /= step  # -100
-            self.max_val /= step  # 100
+            self.min_val /= step
+            self.max_val /= step
             self._adjust_val = step
         else:
             self._adjust_val = 1
@@ -95,18 +91,13 @@ class Slider(QWidget):
         if self._tick_interval is None:
             self._tick_interval = (self.max_val - self.min_val) / 10
 
-        if self.starting_val is None:
-            self.starting_val = self.min_val
-
         self.layout = QVBoxLayout(self)
         self._h_layout = QHBoxLayout()
         self.slider = QSlider(orientation)
         self.slider.setMinimum(self.min_val)
         self.slider.setMaximum(self.max_val)
-
         self.slider.setTickInterval(self._tick_interval)
         self.slider.setTickPosition(QSlider.TicksBelow)
-
         self.slider.setValue(self.starting_val)
 
         # Labels for displaying the range and current value
@@ -118,15 +109,17 @@ class Slider(QWidget):
         self.slider.valueChanged.connect(self.on_value_changed)
         self.slider.valueChanged.connect(lambda value: self.action(self, value))
         self.layout.addWidget(self.value_label)
+        self.layout.addWidget(self.slider)
 
         # Add widgets to layout
         if orientation == Qt.Horizontal:
             self._h_layout.addWidget(self._min_label)
             self._h_layout.addWidget(self._max_label)
-            self.layout.addWidget(self.slider)
-            self.layout.addLayout(self._h_layout)
         else:
-            self.layout.addWidget(self.slider)
+            self._h_layout.addWidget(self._min_label, 0, Qt.AlignTop)
+            self._h_layout.addWidget(self._max_label, 0, Qt.AlignBottom)
+
+        self.layout.addLayout(self._h_layout)
 
     def on_value_changed(self, value):
         """
