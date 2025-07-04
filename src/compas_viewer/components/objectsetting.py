404fb4b26fbca5ec3b610f85e6ecea00ac098f50
Licini <leochen1990@gmail.com>
2025-07-04 19:08:59 +0200

    ordering

--- src/compas_viewer/components/objectsetting.py
+++ src/compas_viewer/components/objectsetting.py
@@ -22,6 +22,10 @@ class ObjectSetting(Component):
         self.widget.setWidgetResizable(True)
         self.reset()
 
+    @property
+    def selected(self):
+        return [obj for obj in self.scene.objects if obj.is_selected]
+
     def reset(self):
         """Reset the content widget and layout to a clean state."""
         self.sub_widgets = {}
@@ -34,11 +38,46 @@ class ObjectSetting(Component):
         self.settings_layout.setContentsMargins(0, 0, 0, 0)
         self.scroll_layout.addLayout(self.settings_layout)
 
-    @property
-    def selected(self):
-        return [obj for obj in self.scene.objects if obj.is_selected]
+    def update(self):
+        """Update the layout with the latest object settings."""
+        self.reset()
 
-    def add_row(self, attr_name: str, widget: QWidget = None) -> None:
+        if len(self.selected) == 1:
+            self._populate()
+            self._add_event_listeners()
+        elif len(self.selected) > 1:
+            self._add_row("Multiple objects selected")
+        else:
+            self._add_row("No object selected")
+
+    def _populate(self) -> None:
+        """Populate the layout with the settings of the selected object."""
+        obj = self.selected[0]
+
+        if not obj:
+            return
+
+        self._add_row("name", TextEdit(text=str(obj.name)))
+
+        if hasattr(obj, "pointcolor") and obj.pointcolor is not None:
+            self._add_row("pointcolor", ColorDialog(obj=obj, attr="pointcolor"))
+
+        if hasattr(obj, "linecolor") and obj.linecolor is not None:
+            self._add_row("linecolor", ColorDialog(obj=obj, attr="linecolor"))
+
+        if hasattr(obj, "facecolor") and obj.facecolor is not None:
+            self._add_row("facecolor", ColorDialog(obj=obj, attr="facecolor"))
+
+        if hasattr(obj, "linewidth") and obj.linewidth is not None:
+            self._add_row("linewidth", DoubleEdit(title=None, value=obj.linewidth, min_val=0.0, max_val=10.0))
+
+        if hasattr(obj, "pointsize") and obj.pointsize is not None:
+            self._add_row("pointsize", DoubleEdit(title=None, value=obj.pointsize, min_val=0.0, max_val=10.0))
+
+        if hasattr(obj, "opacity") and obj.opacity is not None:
+            self._add_row("opacity", DoubleEdit(title=None, value=obj.opacity, min_val=0.0, max_val=1.0))
+
+    def _add_row(self, attr_name: str, widget: QWidget = None) -> None:
         """Create a setting row with label and widget, then add it to the layout."""
         row_layout = QHBoxLayout()
         row_layout.setSpacing(0)
@@ -52,45 +91,6 @@ class ObjectSetting(Component):
             self.sub_widgets[attr_name] = widget
 
         self.settings_layout.addLayout(row_layout)
-
-    def populate(self) -> None:
-        """Populate the layout with the settings of the selected object."""
-        obj = self.selected[0]
-
-        if not obj:
-            return
-
-        self.add_row("name", TextEdit(text=str(obj.name)))
-
-        if hasattr(obj, "pointcolor") and obj.pointcolor is not None:
-            self.add_row("pointcolor", ColorDialog(obj=obj, attr="pointcolor"))
-
-        if hasattr(obj, "linecolor") and obj.linecolor is not None:
-            self.add_row("linecolor", ColorDialog(obj=obj, attr="linecolor"))
-
-        if hasattr(obj, "facecolor") and obj.facecolor is not None:
-            self.add_row("facecolor", ColorDialog(obj=obj, attr="facecolor"))
-
-        if hasattr(obj, "linewidth") and obj.linewidth is not None:
-            self.add_row("linewidth", DoubleEdit(title=None, value=obj.linewidth, min_val=0.0, max_val=10.0))
-
-        if hasattr(obj, "pointsize") and obj.pointsize is not None:
-            self.add_row("pointsize", DoubleEdit(title=None, value=obj.pointsize, min_val=0.0, max_val=10.0))
-
-        if hasattr(obj, "opacity") and obj.opacity is not None:
-            self.add_row("opacity", DoubleEdit(title=None, value=obj.opacity, min_val=0.0, max_val=1.0))
-
-    def update(self):
-        """Update the layout with the latest object settings."""
-        self.reset()
-
-        if len(self.selected) == 1:
-            self.populate()
-            self._add_event_listeners()
-        elif len(self.selected) > 1:
-            self.add_row("Multiple objects selected")
-        else:
-            self.add_row("No object selected")
 
     def _add_event_listeners(self):
         """Add event listeners to the sub widgets."""
